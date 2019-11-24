62aae12f97e05f6c6d35de9a3db5dd26df18cbd5
Dominic Alie <dominic@georadix.com>
2019-11-23 19:55:43 -0500

    Add garage route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { GarageComponent } from './pages/garage/garage.component';
 
 const routes: Routes = [
+  {
+    path: 'garage',
+    component: GarageComponent
+  },
   {
     path: '',
     component: HomeComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './pages/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { GarageComponent } from './pages/garage/garage.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, GarageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
